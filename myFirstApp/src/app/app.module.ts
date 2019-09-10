2186b6f0538ac18b4deb8bdcfe6706a9f61f3d94
Anup Kumar <anupk0792@gmail.com>
2019-09-10 15:48:07 +0530

    Routing & navigation concept

--- myFirstApp/src/app/app.module.ts
+++ myFirstApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
